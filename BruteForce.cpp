53c247531877d3aca56a40f547bdd00f8e5d9926
Hiago Souza <hiago.rafael.souza@hotmail.com>
2022-03-02 14:09:25 -0500

    Update BruteForce.cpp

--- BruteForce.cpp
+++ BruteForce.cpp
@@ -10,24 +10,29 @@ int pickBars(vector<int> bars){
     int max_bars = 0; //var to final result
     
     for(int i=0; i<bars_number; i++){
+        
         count = 0;
         second_basket = -1; //invalid value indicating a free basket
         first_basket = bars[i]; 
         
         for(int j=0;j<bars_number;j++){ //loop throughout the vector
-            if(j>=i) //used to not check previous values
-            {
-                if(first_basket!=bars[j] && second_basket==-1) // second type of bar
+            if(j>=i){ //used to not check previous values
+            
+                if(first_basket!=bars[j] && second_basket==-1){ // second type of bar
                      second_basket = bars[j];
+                }
                 
-                if(first_basket!=bars[j] && second_basket!=bars[j]) //more than 2 types of bars
+                if(first_basket!=bars[j] && second_basket!=bars[j]){ //more than 2 types of bars
                     break;
+                }
                 
                 count++;
             }
         }
-        if(count>max_bars) //brute force check
+        if(count>max_bars){ //brute force check
             max_bars=count;
+        }
+        
     }
     return max_bars; //final result
 }
